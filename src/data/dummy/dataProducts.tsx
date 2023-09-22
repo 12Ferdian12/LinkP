d11a86e8b77657b378ef8e9b56b2bdd55ad18d47
12Ferdian12 <Ferdianadinata07@gmail.com>
2023-09-22 19:55:59 +0700

    feat: search page, loading

--- src/data/dummy/dataProducts.tsx
+++ src/data/dummy/dataProducts.tsx
@@ -5,9 +5,102 @@ export interface DataProduct {
   link: string;
 }
 
-export const dataListProducts: DataProduct[] = [...Array(18)].map((_, i) => ({
-  name: "Baju",
-  image: "/images/img-1.jpeg",
-  price: 100000,
-  link: "https://www.google.com",
-}));
+// export const dataListProducts: DataProduct[] = [...Array(18)].map((_, i) => ({
+//   name: "Baju",
+//   image: "/images/img-1.jpeg",
+//   price: 100000,
+//   link: "https://www.google.com",
+// }));
+
+export const dataListProducts: DataProduct[] = [
+  {
+    name: "Baju",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Kemeja",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Celana",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Sepatu",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Tas",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Jaket",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Kaos kaki",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Meja",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Kursi",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Lemari",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Baju Pria",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Baju Wanita",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Baju Anak",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Baju Muslim",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+  {
+    name: "Baju Muslimah",
+    image: "/images/img-1.jpeg",
+    price: 100000,
+    link: "https://www.google.com",
+  },
+];
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { DataProduct, dataListProducts } from "@/data/dummy/dataProducts";
+import CardProduct from "@/components/cards/cardProduct";
 
 function searchPage() {
-  return <div>searchPage</div>;
+  const router = useRouter();
+  const { keyword } = router.query;
+  const [data, setData] = useState<DataProduct[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function delay() {
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      setIsLoading(false);
+    }
+
+    if (keyword) {
+      setIsLoading(true);
+      setData([]);
+      const data = dataListProducts.filter((item) => {
+        return item.name
+          .toLowerCase()
+          .includes(keyword.toString().toLowerCase());
+      });
+      setData(data);
+      delay();
+    }
+  }, [keyword]);
+
+  return (
+    <div>
+      <h1>Search Page</h1>
+      {isLoading ? (
+        <div className="flex justify-center items-center">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      ) : data.length > 0 ? (
+        data.map((item, i) => {
+          return (
+            <CardProduct
+              image={item.image}
+              name={item.name}
+              price={item.price}
+              link={item.link}
+              key={i}
+            />
+          );
+        })
+      ) : (
+        <p>Not Found</p>
+      )}
+    </div>
+  );
 }
 
 export default searchPage;
